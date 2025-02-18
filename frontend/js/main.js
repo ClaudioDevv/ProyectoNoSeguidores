b800532ea2a3f6469bad0b8e2273535d0cac1426
ClaudioDevv <claudiorivasns@gmail.com>
2025-02-18 17:54:50 +0100

    Cambios en Js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -38,7 +38,7 @@ document.querySelector("#calcularNoSeguidores").addEventListener("click", async 
     formData.append("seguidores", seguidores);
 
     console.log("Enviando datos al backend...");
-    let response = await fetch("https://noseguidores.com/procesar", {
+    let response = await fetch("https://api.noseguidores.com/procesar", {
         method: "POST",
         body: formData
     });
